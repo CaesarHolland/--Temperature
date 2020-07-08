36821e0e00db473a7663158a7c2cf36c1781d448
CaesarHolland <51748713+CaesarHolland@users.noreply.github.com>
2020-07-08 22:39:20 +0800

    modified by cunyu

--- src/action/Test.java
+++ src/action/Test.java
@@ -32,10 +32,6 @@ public class Test extends HttpServlet
 
 	}
 
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
-	{
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
+
 
 }
